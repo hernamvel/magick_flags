279482c1e577db7c3f3e6c45c333ee3517de336b
Hernan Velasquez <hernamvel@gmail.com>
2014-09-16 00:24:35 -0400

    skeleton for japan

--- lib/countries.rb
+++ lib/countries.rb
@@ -1,3 +1,4 @@
 
 require "magick_flags/colombia"
 require "magick_flags/spain"
+require "magick_flags/japan"
---
+++ lib/magick_flags/japan.rb
@@ -0,0 +1,22 @@
+require "magick_flags/version"
+require 'RMagick'
+include Magick
+
+module MagickFlags
+
+  class Spain < MagickFlags::Base
+    
+    def self.draw
+      image = Image.new(@@width,@@height)
+      gc = Magick::Draw.new
+      gc.fill('white')
+      gc.rectangle(0, 0, @@width, @@height)
+      gc.fill('red')
+      gc.circle(@@width/2, @@height/2, @@width/2, @height/4)
+      gc.draw(image)
+      gc.rectangle(0, @@height * 0.75, @@width, @@height)
+      gc.draw(image)
+      image
+    end
+  end
+end
