cafa80056bf8fb34f38ef07b0650f27be02609a1
Hernan Velasquez <hernamvel@gmail.com>
2014-09-19 00:02:41 -0400

    denmark flag skeleton

--- lib/countries.rb
+++ lib/countries.rb
@@ -1,6 +1,7 @@
 
 require "magick_flags/austria"
 require "magick_flags/colombia"
+require "magick_flags/denmark"
 require "magick_flags/england"
 require "magick_flags/japan"
 require "magick_flags/spain"
---
+++ lib/magick_flags/denmark.rb
@@ -0,0 +1,21 @@
+require "magick_flags/version"
+require 'RMagick'
+include Magick
+
+module MagickFlags
+
+  class England < MagickFlags::Base
+    
+    def self.draw
+      image = Image.new(@@width,@@height)
+      gc = Magick::Draw.new
+      gc.fill('red')
+      gc.rectangle(0, 0, @@width, @@height)
+      gc.fill('white')
+      gc.rectangle(@@width * 0.30, 0, @@width * 0.35, @@height)
+      gc.rectangle(0, @@height * 0.44, @@width, @@height * 0.56)
+      gc.draw(image)
+      image
+    end
+  end
+end
