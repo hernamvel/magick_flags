34bd31a0622e243a52d316fd183fd4cdd35380b9
Hernan Velasquez <hernamvel@gmail.com>
2014-10-05 00:13:17 -0400

    nigeria skeleton

---
+++ lib/magick_flags/nigeria.rb
@@ -0,0 +1,22 @@
+require "magick_flags/version"
+require 'RMagick'
+include Magick
+
+module MagickFlags
+
+  class Nigeria < MagickFlags::Base
+
+    def self.draw
+      image = Image.new(@@width,@@height)
+      gc = Magick::Draw.new
+      gc.fill('blue')
+      gc.rectangle(0, 0, @@width * 0.33, @@height)
+      gc.fill('white')
+      gc.rectangle(@@width * 0.33, 0, @@width * 0.66, @@height)
+      gc.fill('red')
+      gc.rectangle(@@width * 0.66, 0, @@width, @@height)
+      gc.draw(image)
+      image
+    end
+  end
+end
